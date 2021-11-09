ba96a448ead18774d37fe43a8cf4858d16f09e04
martin89tricolor <66755493+martin89tricolor@users.noreply.github.com>
2021-11-09 18:38:01 -0300

    Add files via upload

---
+++ controllers/products.controller.js
@@ -0,0 +1,141 @@
+var ProductService = require('../services/product.service');
+
+// Saving the context of this module inside the _the variable
+_this = this;
+
+// Async Controller function to get the To do List
+exports.getProducts = async function (req, res, next) {
+
+    // Check the existence of the query parameters, If doesn't exists assign a default value
+    var pageNumber = req.query.page ? req.query.page : 1
+    var limitNumber = req.query.limit ? Number(req.query.limit) : 8;
+    const {page, limit, ordenamiento, ...queryParams} = req.query;
+    if(queryParams.nombre) {
+        queryParams.nombre = new RegExp(queryParams.nombre, "gi");
+    }
+    try {
+        var Products = await ProductService.getProducts(queryParams, ordenamiento, pageNumber, limitNumber)
+        // Requiere ordenamiento?
+        // Return the Users list with the appropriate HTTP password Code and Message.
+        return res.status(200).json({status: 200, data: Products, message: "Succesfully Products Recieved"});
+    } catch (e) {
+        //Return an Error Response Message with Code and the Error Message.
+        return res.status(400).json({status: 400, message: e.message});
+    }
+}
+
+exports.getLatestProducts = async function (req, res, next) {
+
+    try {
+        var Products = await ProductService.getLatestProducts()
+        // Return the Users list with the appropriate HTTP password Code and Message.
+        return res.status(200).json({status: 200, data: Products, message: "Succesfully Products Recieved"});
+    } catch (e) {
+        //Return an Error Response Message with Code and the Error Message.
+        return res.status(400).json({status: 400, message: e.message});
+    }
+}
+
+exports.getLatestEmbotellados = async function (req, res, next) {
+
+    try {
+        var Products = await ProductService.getLatestEmbotellados()
+        // Return the Users list with the appropriate HTTP password Code and Message.
+        return res.status(200).json({status: 200, data: Products, message: "Succesfully Products Recieved"});
+    } catch (e) {
+        //Return an Error Response Message with Code and the Error Message.
+        return res.status(400).json({status: 400, message: e.message});
+    }
+}
+
+exports.getLatestEnlatados = async function (req, res, next) {
+
+    try {
+        var Products = await ProductService.getLatestEnlatados()
+        // Return the Users list with the appropriate HTTP password Code and Message.
+        return res.status(200).json({status: 200, data: Products, message: "Succesfully Products Recieved"});
+    } catch (e) {
+        //Return an Error Response Message with Code and the Error Message.
+        return res.status(400).json({status: 400, message: e.message});
+    }
+}
+
+exports.getProductById = async function (req, res, next) {
+
+    // Check the existence of the query parameters, If doesn't exists assign a default value
+    const filtro = req.params.id;
+    try {
+        var Product = await ProductService.getProductById(filtro);
+        // Return the Users list with the appropriate HTTP password Code and Message.
+        return res.status(200).json({status: 200, data: Product, message: "Succesfully Product Recieved"});
+    } catch (e) {
+        //Return an Error Response Message with Code and the Error Message.
+        return res.status(400).json({status: 400, message: e.message});
+    }
+}
+
+exports.deleteProductsById = async function (req, res, next) {
+
+    // Check the existence of the query parameters, If doesn't exists assign a default value
+    const ids = req.body.ids;
+    try {
+        const results = await ProductService.deleteProductsById(ids);
+        const message = results.deletedCount === 1 ? `Se borró 1 producto` : `Se borraron ${results.deletedCount} productos`;
+        // Return the Users list with the appropriate HTTP password Code and Message.
+        return res.status(200).json({
+            status: 200,
+            data: results,
+            message,
+        });
+    } catch (e) {
+        //Return an Error Response Message with Code and the Error Message.
+        return res.status(400).json({status: 400, message: e.message});
+    }
+}
+
+exports.updateProduct = async function (req, res, next) {
+
+    // Id is necessary for the update
+    if (!req.body._id) {
+        return res.status(400).json({status: 400., message: "ID must be present"})
+    }
+
+    var Product = {
+        ...req.body,
+    }
+    try {
+        var updatedProduct = await ProductService.updateProduct(Product);
+        return res.status(200).json({status: 200, data: updatedProduct, message: "Producto modificado."})
+    } catch (e) {
+        return res.status(400).json({status: 400, message: e.message})
+    }
+}
+
+exports.createProduct = async function (req, res, next) {
+    // Req.Body contains the form submit values.
+    var Product = {
+        ...req.body
+    };
+    try {
+        // Calling the Service function with the new object from the Request Body
+        var createdProduct = await ProductService.createProduct(Product)
+        return res.status(201).json({status: 201, data: createdProduct, message: "Succesfully Created Product"})
+    } catch (e) {
+        //Return an Error Response Message with Code and the Error Message.
+        console.log(e)
+        return res.status(400).json({status: 400, message: "Product Creation was Unsuccesfull"})
+    }
+}
+
+exports.getFilters = async function (req, res, next) {
+
+    // Check the existence of the query parameters, If doesn't exists assign a default value
+    try {
+        var Filters = await ProductService.getFilters();
+        // Return the Users list with the appropriate HTTP password Code and Message.
+        return res.status(200).json({status: 200, data: Filters, message: "Succesfully Filters Recieved"});
+    } catch (e) {
+        //Return an Error Response Message with Code and the Error Message.
+        return res.status(400).json({status: 400, message: e.message});
+    }
+}
---
+++ controllers/users.controller.js
@@ -0,0 +1,194 @@
+var UserService = require('../services/user.service');
+
+// Saving the context of this module inside the _the variable
+_this = this;
+
+// Async Controller function to get the To do List
+exports.getUsers = async function (req, res, next) {
+
+    // Check the existence of the query parameters, If doesn't exists assign a default value
+    var page = req.query.page ? req.query.page : 1
+    var limit = req.query.limit ? req.query.limit : 10;
+    try {
+        var Users = await UserService.getUsers({}, page, limit)
+        // Return the Users list with the appropriate HTTP password Code and Message.
+        return res.status(200).json({status: 200, data: Users, message: "Succesfully Users Recieved"});
+    } catch (e) {
+        //Return an Error Response Message with Code and the Error Message.
+        return res.status(400).json({status: 400, message: e.message});
+    }
+}
+
+exports.getActualUser = async function (req, res, next) {
+
+    // Check the existence of the query parameters, If doesn't exists assign a default value
+    try {
+        var User = await UserService.getUserById(req.userId);
+        // Return the Users list with the appropriate HTTP password Code and Message.
+        return res.status(200).json({status: 200, data: User, message: "Succesfully User Recieved"});
+    } catch (e) {
+        //Return an Error Response Message with Code and the Error Message.
+        return res.status(400).json({status: 400, message: e.message});
+    }
+}
+
+exports.getUserById = async function (req, res, next) {
+
+    // Check the existence of the query parameters, If doesn't exists assign a default value
+    const filtro = req.params.id;
+    try {
+        var User = await UserService.getUserById(filtro);
+        // Return the Users list with the appropriate HTTP password Code and Message.
+        return res.status(200).json({status: 200, data: User, message: "Succesfully User Recieved"});
+    } catch (e) {
+        //Return an Error Response Message with Code and the Error Message.
+        return res.status(400).json({status: 400, message: e.message});
+    }
+}
+
+exports.getUsersByMail = async function (req, res, next) {
+
+    // Check the existence of the query parameters, If doesn't exists assign a default value
+    var page = req.query.page ? req.query.page : 1
+    var limit = req.query.limit ? req.query.limit : 10;
+    let filtro= {email: req.body.email}
+    try {
+        var Users = await UserService.getUsers(filtro, page, limit)
+        // Return the Users list with the appropriate HTTP password Code and Message.
+        return res.status(200).json({status: 200, data: Users, message: "Succesfully Users Recieved"});
+    } catch (e) {
+        //Return an Error Response Message with Code and the Error Message.
+        return res.status(400).json({status: 400, message: e.message});
+    }
+}
+
+exports.createUser = async function (req, res, next) {
+    // Req.Body contains the form submit values.
+    var User = {
+        comertialName: req.body.comertialName,
+        email: req.body.email,
+        password: req.body.password,
+        address1: req.body.address1,
+        province: req.body.province,
+        city: req.body.city,
+        phone: req.body.phone,
+    }
+    try {
+        // Calling the Service function with the new object from the Request Body
+        var createdUser = await UserService.createUser(User)
+        return res.status(201).json({createdUser, message: "Succesfully Created User"})
+    } catch (e) {
+        //Return an Error Response Message with Code and the Error Message.
+        console.log(e)
+        return res.status(400).json({status: 400, message: "User Creation was Unsuccesfull"})
+    }
+}
+
+exports.updateUser = async function (req, res, next) {
+
+    // Id is necessary for the update
+    if (!req.body._id) {
+        return res.status(400).json({status: 400., message: "ID must be present"})
+    }
+
+    var User = {
+        ...req.body,
+    }
+    try {
+        var updatedUser = await UserService.updateUser(User)
+        return res.status(200).json({status: 200, data: updatedUser, message: "Usuario modificado."})
+    } catch (e) {
+        return res.status(400).json({status: 400., message: e.message})
+    }
+}
+
+exports.updateActualUser = async function (req, res, next) {
+
+    // Id is necessary for the update
+    if (!req.body._id) {
+        return res.status(400).json({status: 400., message: "ID must be present"})
+    }
+    if(req.body._id != req.userId) {
+        return res.status(400).json({status: 400., message: "Solo podés actualizar los datos del usuario actual!"});
+    }
+
+    var User = {
+        ...req.body,
+    }
+    try {
+        var updatedUser = await UserService.updateUser(User)
+        return res.status(200).json({status: 200, data: updatedUser, message: "Usuario modificado."})
+    } catch (e) {
+        return res.status(400).json({status: 400., message: e.message})
+    }
+}
+
+exports.removeUser = async function (req, res, next) {
+
+    var id = req.params.id;
+    try {
+        var deleted = await UserService.deleteUser(id);
+        res.status(200).send("Succesfully Deleted... ");
+    } catch (e) {
+        return res.status(400).json({status: 400, message: e.message})
+    }
+}
+
+exports.removeUsers = async function (req, res, next) {
+
+    // Check the existence of the query parameters, If doesn't exists assign a default value
+    const ids = req.body.ids;
+    try {
+        const results = await UserService.deleteUsersById(ids);
+        const message = results.deletedCount === 1 ? `Se borró 1 usuario` : `Se borraron ${results.deletedCount} usuarios`;
+        // Return the Users list with the appropriate HTTP password Code and Message.
+        return res.status(200).json({
+            status: 200,
+            data: results,
+            message,
+        });
+    } catch (e) {
+        //Return an Error Response Message with Code and the Error Message.
+        return res.status(400).json({status: 400, message: e.message});
+    }
+}
+
+
+exports.loginUser = async function (req, res, next) {
+    // Req.Body contains the form submit values.
+    var User = {
+        email: req.body.email,
+        password: req.body.password
+    }
+    try {
+        // Calling the Service function with the new object from the Request Body
+        var loginUser = await UserService.loginUser(User);
+        return res.status(200).json({loginUser, message: "Succesfully login"})
+    } catch (e) {
+        //Return an Error Response Message with Code and the Error Message.
+        return res.status(400).json({status: 400, message: "Invalid username or password"})
+    }
+}
+
+exports.getImagenUserByMail = async function (req, res, next) {
+
+    // Check the existence of the query parameters, If doesn't exists assign a default value
+    var page = req.query.page ? req.query.page : 1
+    var limit = req.query.limit ? req.query.limit : 10;
+    //obtener filtro
+    var filtro = {
+        mail: req.body.email
+    }
+    try {
+        var UsersImg = await UserImgService.getImagenesByUser(filtro, page, limit)
+        // Return the Users list with the appropriate HTTP password Code and Message.
+        if (UsersImg.total===0)
+            return res.status(201).json({status: 201, data: UsersImg, message: "No existe Mail"});
+        else
+            return res.status(200).json({status: 200, data: UsersImg, message: "Succesfully Users Recieved"});
+    } catch (e) {
+        //Return an Error Response Message with Code and the Error Message.
+        console.log(e)
+        return res.status(400).json({status: 400, message: e.message});
+    }
+}
