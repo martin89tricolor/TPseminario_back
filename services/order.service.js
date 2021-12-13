6cfedfbe055b0019ada5fbc624affe773430d948
martin89tricolor <66755493+martin89tricolor@users.noreply.github.com>
2021-12-12 23:00:32 -0300

    Add files via upload

---
+++ services/order.service.js
@@ -0,0 +1,92 @@
+// Gettign the Newly created Mongoose Model we just created
+var Order = require('../models/Order.model');
+
+// Saving the context of this module inside the _the variable
+_this = this
+
+// Async function to get the User List
+exports.getOrders = async function (query, page, limit) {
+
+    // Options setup for the mongoose paginate
+    var options = {
+        page,
+        limit,
+        sort: {$natural: -1},
+    }
+    // Try Catch the awaited promise to handle the error
+    try {
+        console.log("Query",query)
+        var Orders = await Order.paginate(query, options)
+        // Return the Userd list that was retured by the mongoose promise
+        return Orders;
+
+    } catch (e) {
+        // return a Error message describing the reason
+        console.log("error services",e)
+        throw Error('Error while Paginating Orders');
+    }
+}
+
+exports.getOrderById = async function (id) {
+
+    // Try Catch the awaited promise to handle the error
+    try {
+        var OrderResult = await Order.findById(id);
+        return OrderResult;
+
+    } catch (e) {
+        // return a Error message describing the reason
+        console.log("error services",e)
+        throw Error('Error while retrieving Order');
+    }
+}
+
+exports.createOrder = async function (order) {
+    var newOrder = new Order({
+        ...order,
+    });
+
+    try {
+        // Saving the Order
+        var savedOrder = await newOrder.save();
+        return savedOrder;
+    } catch (e) {
+        // return a Error message describing the reason
+        console.log(e)
+        throw Error("Error while Creating Order")
+    }
+}
+
+exports.updateOrderStatus = async function (ids, nuevoEstado) {
+
+    // Try Catch the awaited promise to handle the error
+    try {
+        var OrderResult = await Order.updateMany(
+            {'_id': {$in: ids}},
+            {estado: nuevoEstado}
+        );
+        return OrderResult;
+
+    } catch (e) {
+        // return a Error message describing the reason
+        console.log("error services",e)
+        throw Error('Error while updating Orders');
+    }
+}
+
+exports.updateOrderStatus2 = async function (ids, nuevoEstado) {
+
+    // Try Catch the awaited promise to handle the error
+    try {
+        var OrderResult = await Order.updateMany(
+            {'_id': {$in: ids}},
+            {estado2: nuevoEstado}
+        );
+        return OrderResult;
+
+    } catch (e) {
+        // return a Error message describing the reason
+        console.log("error services",e)
+        throw Error('Error while updating Orders');
+    }
+}
